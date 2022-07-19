4b670186fe05ccc916e6b7d7dd9eb99ed8715d44
mbatarelo <mbatarelo@hattrick.hr>
2022-07-19 12:56:32 +0200

    Instaling external packages
    string interpolation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './transaction.dart';
 
 void main() => runApp(MyApp());
@@ -55,7 +56,7 @@ class MyHomePage extends StatelessWidget {
                     Container(
                       margin: EdgeInsets.symmetric(
                         vertical: 10,
-                        horizontal: 30,
+                        horizontal: 15,
                       ),
                       decoration: BoxDecoration(
                           border: Border.all(
@@ -63,7 +64,7 @@ class MyHomePage extends StatelessWidget {
                         width: 2,
                       )),
                       padding: EdgeInsets.all(10),
-                      child: Text(tx.amount.toString(),
+                      child: Text("\$${tx.amount}",
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
@@ -79,7 +80,7 @@ class MyHomePage extends StatelessWidget {
                               fontSize: 16, fontWeight: FontWeight.bold),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat.yMMMMd().format(tx.date),
                           style: TextStyle(color: Colors.grey),
                         ),
                       ],
